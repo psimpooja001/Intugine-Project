8456e922d65e0c46af0ba3506de6857b4e50a9a6
Poo <poo@example.com>
2021-01-02 20:48:13 +0530

    Bootstrapped reactJS project from scratch

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import Home from './components/Home';
+import { Transporters } from './components/Transporters';
+import { Brands } from './components/Brands';
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/brands' component={Brands} />
+        <Route path='/transporters' component={Transporters} />
+      </Layout>
+    );
+  }
+}
